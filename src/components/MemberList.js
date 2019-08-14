404a94d8365007a7991361105d011aa5a035c7d2
benjamin-t-wilson <benjiimanwillson@gmail.com>
2019-08-13 18:02:37 -0700

    changed default team member to match ipsum

--- src/components/MemberList.js
+++ src/components/MemberList.js
@@ -1,8 +1,8 @@
 const MemberList = [
   {
     id: 1,
-    name: "Sylvester Calzone",
-    occupation: "Movie Star",
+    name: "Tonald Drump",
+    occupation: "Leader of the Free World",
     bio: `I was going to say something extremely rough to Lorem Ipsum, to its family, and I said to myself, "I can't do it. I just can't do it. It's inappropriate. It's not nice." Lorem Ipsum better hope that there are no "tapes" of our conversations before he starts leaking to the press! Lorem Ipsum's father was with Lee Harvey Oswald prior to Oswald's being, you know, shot. This placeholder text is gonna be HUGE.`
   }
 ];
