ea6bfb96cce5423ac19c1534c98a7c2ab32957b9
benjamin-t-wilson <benjiimanwillson@gmail.com>
2019-08-18 17:27:23 -0700

    attempting to make publish to netlify functional

---
+++ build/precache-manifest.fc207f41ac402b4d4ade8e73b6e8e876.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c72c5f31b4e3688e653c4460ae03a9dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91fab3436a9813fee9a5",
+    "url": "/static/css/main.f5b0c987.chunk.css"
+  },
+  {
+    "revision": "8d11ba20fe9007ccc9b9",
+    "url": "/static/js/2.b2875796.chunk.js"
+  },
+  {
+    "revision": "91fab3436a9813fee9a5",
+    "url": "/static/js/main.65acefd6.chunk.js"
+  },
+  {
+    "revision": "7769e8367e852fb12afa",
+    "url": "/static/js/runtime~main.42ac24a6.js"
+  }
+]);
